0a525e9fc91aecc0a9a18b7fd5e868dab5e0b73e
Oscardenas1 <oscarfish1000@gmail.com>
2021-11-21 21:14:14 -0600

    c7

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,9 @@
-
 // Responsive Function for the Navigation Bar
 function RNavBar() {
- var x = document.getElementById("TopNavBar");
- if (x.className === "NavBar") {
-   x.className += " responsive";
- } else {
-   x.className = "NavBar";
- }
+  var x = document.getElementById("TopNavBar");
+  if (x.className === "NavBar") {
+    x.className += " responsive";
+  } else {
+    x.className = "NavBar";
+  }
 }
